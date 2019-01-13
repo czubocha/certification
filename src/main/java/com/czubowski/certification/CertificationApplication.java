acfae7b63db6b4fa6e0c08fdab31b54815cd6659
Tomasz Czubocha <tomasz.czubocha@gmail.com>
2019-01-13 02:41:51 +0100

    init

---
+++ src/main/java/com/czubowski/certification/CertificationApplication.java
@@ -0,0 +1,14 @@
+package com.czubowski.certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertificationApplication.class, args);
+    }
+
+}
+
