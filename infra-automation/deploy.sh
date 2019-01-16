4c7caee050dc5646f396d76887a86b8609ec3606
Tomasz Czubocha <czubochat@ryanair.com>
2019-01-16 04:06:17 +0100

    add infra and Dockerfile

---
+++ infra-automation/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+export accound_id=071572870590
+export infra_bucket_name=certification-infra-auto
+export region=eu-central-1
+export mysshkey=certification
+aws cloudformation update-stack --stack-name certification-cicd --template-url \
+https://s3-$region.amazonaws.com/$infra_bucket_name/master-ecs.yaml --parameters \
+ParameterKey=CodeBuildContainerSpringBootDocker,ParameterValue=$account_id.dkr.ecr.$region.amazonaws.com/custombuild:latest \
+ParameterKey=InfraAutomationCfnBucket,ParameterValue=$infra_bucket_name \
+ParameterKey=KeyPair,ParameterValue=$mysshkey \
+--capabilities CAPABILITY_IAM
